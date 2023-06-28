637d0f939fb919b07d5364811535e4f13b928fb2
k_senda <k_senda@yumemi.co.jp>
2023-06-28 14:50:55 +0900

    add: MainScreen

---
+++ app/src/main/java/jp/co/yumemi/dynamictextsample/ui/MainScreen.kt
@@ -0,0 +1,55 @@
+package jp.co.yumemi.dynamictextsample.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jp.co.yumemi.dynamictextsample.R
+import jp.co.yumemi.dynamictextsample.ui.theme.DynamicTextSampleTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                    )
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+        ) {
+            MessageSection(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(20.dp),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMainScreen() {
+    DynamicTextSampleTheme {
+        MainScreen()
+    }
+}
---
+++ app/src/main/java/jp/co/yumemi/dynamictextsample/ui/MessageSection.kt
@@ -0,0 +1,50 @@
+package jp.co.yumemi.dynamictextsample.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jp.co.yumemi.dynamictextsample.R
+import jp.co.yumemi.dynamictextsample.ui.theme.DynamicTextSampleTheme
+
+@Composable
+fun MessageSection(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Hello, Android!",
+            style = MaterialTheme.typography.titleLarge,
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_android),
+            contentDescription = null,
+            modifier = Modifier
+                .size(180.dp)
+                .padding(all = 10.dp),
+        )
+        Text(
+            text = "This is a sample app, where displayed text can be changed dynamically.",
+            style = MaterialTheme.typography.bodyLarge,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageSection() {
+    DynamicTextSampleTheme {
+        MessageSection()
+    }
+}
