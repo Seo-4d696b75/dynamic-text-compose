2974173d1d6bccb403a6dd5f777ac130d009ecb5
k_senda <k_senda@yumemi.co.jp>
2023-06-28 18:46:05 +0900

    fix: TextId

--- app/src/main/java/jp/co/yumemi/dynamictextsample/domain/TextId.kt
+++ app/src/main/java/jp/co/yumemi/dynamictextsample/domain/TextId.kt
@@ -1,13 +1,19 @@
 package jp.co.yumemi.dynamictextsample.domain
 
-enum class TextId {
-    appBarTilte,
-    homeTitle,
-    homeBody,
-    settingTitle,
-    settingButtonLabel,
-    languageEnglish,
-    langaugeJapanese,
-    languageFrench,
-    langaugeThai,
+import androidx.annotation.StringRes
+import jp.co.yumemi.dynamictextsample.R
+
+@Suppress("EnumEntryName")
+enum class TextId(
+   @StringRes val xmlId: Int
+) {
+    app_bar_title(R.string.app_bar_title),
+    home_title(R.string.home_title),
+    home_body(R.string.home_body),
+    setting_title(R.string.setting_title),
+    setting_button(R.string.setting_button),
+    language_english(R.string.language_english),
+    language_japanese(R.string.language_japanese),
+    language_french(R.string.language_french),
+    language_thai(R.string.language_thai),
 }
