2c8c0d2ec2c942adadfa8c6aad0a8ae136957de5
k_senda <k_senda@yumemi.co.jp>
2023-06-28 18:21:15 +0900

    fix: repository impl

--- app/src/main/java/jp/co/yumemi/dynamictextsample/data/DisplayLanguageRepositoryImpl.kt
+++ app/src/main/java/jp/co/yumemi/dynamictextsample/data/DisplayLanguageRepositoryImpl.kt
@@ -4,17 +4,48 @@ import android.content.Context
 import dagger.hilt.android.qualifiers.ApplicationContext
 import jp.co.yumemi.dynamictextsample.domain.DisplayLanguage
 import jp.co.yumemi.dynamictextsample.domain.DisplayLanguageRepository
+import jp.co.yumemi.dynamictextsample.domain.TextId
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.builtins.MapSerializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
 import javax.inject.Inject
 
 class DisplayLanguageRepositoryImpl @Inject constructor(
-    @ApplicationContext context: Context,
+    @ApplicationContext private val context: Context,
 ) : DisplayLanguageRepository {
-    private val _displayLanguage = MutableStateFlow(DisplayLanguage.English)
-    override val displayLanguage = _displayLanguage.asStateFlow()
+    private val _provider = MutableStateFlow(
+        TextProviderImpl(
+            language = DisplayLanguage.English,
+            data = emptyMap(),
+        )
+    )
+    override val provider = _provider.asStateFlow()
 
+    @OptIn(ExperimentalSerializationApi::class)
     override fun onLanguageChanged(language: DisplayLanguage) {
-        TODO("Not yet implemented")
+        val fileName = "text_${language.name.lowercase()}.json"
+        val serializer = MapSerializer(String.serializer(), String.serializer())
+        val map = context
+            .assets
+            .open(fileName)
+            .use {
+                Json.decodeFromStream(serializer, it)
+            }
+            .entries
+            .associate { entry ->
+                val id = TextId.values().first { it.name == entry.key }
+                id to entry.value
+            }
+        _provider.update {
+            TextProviderImpl(
+                language = language,
+                data = map,
+            )
+        }
     }
 }
